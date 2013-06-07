135de55384d0257bee4ba82d7472ca6e7f54ebe6
Sivan Sun <sun.sivan@gmail.com>
2013-06-07 12:26:07 +0800

    v1.0.0 stable version
    Update normalize version to 2.1.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,22 +12,12 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		cssmin: {
-			minify: {
-				expand: true,
-				cwd: 'css',
-				src: ['*.css', '!*.min.css', '!_*.css'],
-				dest: 'css',
-				ext: '.min.css'
-			}
-		},
 		watch: {
 			files: ['sass/*.sass'],
 			tasks: ['compass']
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['compass', 'watch']);
